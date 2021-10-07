e983435a898d69d0bf51baafb330c0ac049edf1f
Guilherme Rodriguero <gui.rodriguero@hotmail.com>
2021-10-07 01:54:55 -0300

    Expense-Tracker project

---
+++ Day2-FinancialControl/expense-tracker/src/App.tsx
@@ -0,0 +1,17 @@
+import * as C from './App.styles';
+
+const App = () => {
+  return(
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        ...
+        {/*Parei em 25:50 */}
+      </C.Body>
+    </C.Container>
+  );
+}
+
+export default App;
