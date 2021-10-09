82a30da9f9ae7f26078de221eb91423ba9ebc88d
Guilherme Rodriguero <gui.rodriguero@hotmail.com>
2021-10-09 17:27:10 -0300

    Gallery done

--- Day3-Galery/galery/src/libs/firebase.ts
+++ Day3-Galery/galery/src/libs/firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE,
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEAGINGSENDERID,
     appId: process.env.REACT_APP_FIREBASE_APPID
-  };
+};
 
-  const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
-  export const storage = getStorage(firebaseApp);
+export const storage = getStorage(firebaseApp);
