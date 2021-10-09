9bfd05f2db233dfc2d6b807cf7cffaa3aeb78834
Guilherme Rodriguero <gui.rodriguero@hotmail.com>
2021-10-09 03:25:24 -0300

    Firebase config

--- Day3-Galery/galery/src/App.tsx
+++ Day3-Galery/galery/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {//parei 22:15
   return (
     <div className="App">
       <header className="App-header">
